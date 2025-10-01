6258efcbd4a16d3d17c0a464459bfcba27166d6e
uij <171641141+uij-dev@users.noreply.gi>
2025-10-01 03:08:35 +0100

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,15 +2,24 @@ import express from 'express';
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+// make ready for deployment
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
+    app.get("*", (_, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    })
+}
 app.listen(3000, () => console.log("server is running on port 3000"));
