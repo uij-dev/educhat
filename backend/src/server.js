53622b2184bcc5e14551c507bd1b5b4bd83a3313
uij <171641141+uij-dev@users.noreply.gi>
2025-10-04 04:15:21 +0100

    update profile endpoint added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import dotenv from "dotenv";
+import path from "path";
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import path from "path";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js"
 
@@ -12,6 +14,7 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json()); // req.body
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
