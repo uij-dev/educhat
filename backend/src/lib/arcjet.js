6f504e26bb6c3a830e53b7cdbbc9331c570788cf
uij <171641141+uij-dev@users.noreply.gi>
2025-10-05 03:43:17 +0100

    rate limiting implemented

---
+++ backend/src/lib/arcjet.js
@@ -0,0 +1,74 @@
+import arcjet, { shield, detectBot, slidingWindow } from "@arcjet/node";
+import { isSpoofedBot } from "@arcjet/inspect";
+import express from "express";
+import { ENV } from "./env.js";
+
+const app = express();
+const port = 3000;
+
+const aj = arcjet({
+    key: ENV.ARCJET_KEY,
+    rules: [
+        // Shield protects your app from common attacks e.g. SQL injection
+        shield({ mode: "LIVE" }),
+        // Create a bot detection rule
+        detectBot({
+            mode: "LIVE", // Blocks requests. Use "DRY_RUN" to log only
+            // Block all bots except the following
+            allow: [
+                "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
+                // Uncomment to allow these other common bot categories
+                // See the full list at https://arcjet.com/bot-list
+                //"CATEGORY:MONITOR", // Uptime monitoring services
+                //"CATEGORY:PREVIEW", // Link previews e.g. Slack, Discord
+            ],
+        }),
+        // Create a token bucket rate limit. Other algorithms are supported.
+        slidingWindow({
+            mode: "LIVE",
+            max: 100,
+            interval:60,
+        })
+    ],
+});
+
+// app.get("/", async (req, res) => {
+//     const decision = await aj.protect(req, { requested: 5 }); // Deduct 5 tokens from the bucket
+//     console.log("Arcjet decision", decision);
+
+//     if (decision.isDenied()) {
+//         if (decision.reason.isRateLimit()) {
+//             res.writeHead(429, { "Content-Type": "application/json" });
+//             res.end(JSON.stringify({ error: "Too Many Requests" }));
+//         } else if (decision.reason.isBot()) {
+//             res.writeHead(403, { "Content-Type": "application/json" });
+//             res.end(JSON.stringify({ error: "No bots allowed" }));
+//         } else {
+//             res.writeHead(403, { "Content-Type": "application/json" });
+//             res.end(JSON.stringify({ error: "Forbidden" }));
+//         }
+//     } else if (decision.ip.isHosting()) {
+//         // Requests from hosting IPs are likely from bots, so they can usually be
+//         // blocked. However, consider your use case - if this is an API endpoint
+//         // then hosting IPs might be legitimate.
+//         // https://docs.arcjet.com/blueprints/vpn-proxy-detection
+//         res.writeHead(403, { "Content-Type": "application/json" });
+//         res.end(JSON.stringify({ error: "Forbidden" }));
+//     } else if (decision.results.some(isSpoofedBot)) {
+//         // Paid Arcjet accounts include additional verification checks using IP data.
+//         // Verification isn't always possible, so we recommend checking the decision
+//         // separately.
+//         // https://docs.arcjet.com/bot-protection/reference#bot-verification
+//         res.writeHead(403, { "Content-Type": "application/json" });
+//         res.end(JSON.stringify({ error: "Forbidden" }));
+//     } else {
+//         res.writeHead(200, { "Content-Type": "application/json" });
+//         res.end(JSON.stringify({ message: "Hello World" }));
+//     }
+// });
+
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`);
+// });
+
+export default aj;
