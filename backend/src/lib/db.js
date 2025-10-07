64de4bc5ffab17fefd263956244fae7b387131dc
uij <171641141+uij-dev@users.noreply.gi>
2025-10-07 03:28:57 +0100

    message routes added

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -4,7 +4,6 @@ import {ENV} from "./env.js"
 export const connectDB = async () => {
     try {
         const {MONGO_URI} = ENV;
-        
         if (!MONGO_URI) throw new Error("MONGO_URI is not set");
         
         const conn = await mongoose.connect(ENV.MONGO_URI);
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,19 @@
 import express from "express";
+import { getAllContacts, getChatPartners, getMessagesByUserId, sendMessage } from "../controllers/message.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = new express.Router();
 
-router.get("/send", (req, res) => {
-    res.send("Send message endpoint");
-})
+
+// the middlewares execute in order - so requests get rete-limited first, then authenticated.
+// this is actually more efficient since unauthenticated requests get blocked by rate limiting before hitting the auth middleware.
+
+router.use(arcjetProtection, protectRoute)
+
+router.get("/contacts", getAllContacts)
+router.get("/chats",protectRoute, getChatPartners)
+router.get("/:id", getMessagesByUserId)
+router.post("/send/:id", sendMessage)
 
 export default router;
