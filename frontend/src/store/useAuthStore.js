b8718d30c8cd879e59902768f26416c196b3221b
uij <171641141+uij-dev@users.noreply.gi>
2025-10-13 08:23:46 +0100

    login and logout implemented

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -6,6 +6,7 @@ export const useAuthStore = create((set) => ({
     authUser: null,
     isCheckingAuth: true,
     isSigningUp: false,
+    isLoggingIn: false,
 
 
     checkAuth: async () => {
@@ -31,6 +32,31 @@ export const useAuthStore = create((set) => ({
             toast.error(error.response.data.message)
         } finally {
             set({ isSigningUp: false })
+        }
+    },
+    
+     login: async (data) => {
+        set({ isLoggingIn: true })
+        try {
+            const res = await axiosInstance.post("./auth/login", data);
+            set({ authUser: res.data });
+
+            toast.success("Logged in successfully");
+        } catch (error) {
+            toast.error(error.response.data.message)
+        } finally {
+            set({ isLoggingIn: false })
+        }
+    },
+
+    logout: async() => {
+        try {
+            await axiosInstance.post("/auth/logout");
+            set({ authUser: null });
+            toast.success("Logged out successfully");
+        } catch (error) {
+            toast.error("Error in logging out");
+            console.log("Logout error:", error);
         }
     }
 }))
